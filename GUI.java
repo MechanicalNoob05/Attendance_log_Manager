2b87af12593136c8bc5a8a3c61fcd7f8df7b01a1
Tejas Mayekar <35838069+tejas12345678@users.noreply.github.com>
2022-01-28 17:28:40 +0530

    Button and text box added to mansi window
    next taget history window

--- GUI.java
+++ GUI.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.*;  
+import java.awt.*;
 public class GUI{
-	JFrame window;
-	JPanel panel,panel1,panel2,panel3,panel4;
+	JFrame window,frame;
+	JPanel panel,panel1,panel2,panel3,panel4,ADSnames;
 	JLabel name,Roll,Date,tab3,tab4,D,N,R,Roll1,PArb,ANo,SELDEP,SELDIV,SELNS;
 	JTabbedPane tp,tp1;
-	JTextField DEPNAME,DEPND,DEPNS;
+	JTextField DEPNAME,DEPND,DEPNS,EntName;
 	public static void main(String[] args){
 		new GUI();
 	}
@@ -24,33 +24,35 @@ public class GUI{
 		window = new JFrame("Test");
 		window.setSize(800,600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame = new JFrame("mansi");
+		frame.setSize(400, 400);
 	}
 	public void tabbedPane(){
 		tp=new JTabbedPane();
-		tp1=new JTabbedPane();  
+		tp1=new JTabbedPane();
 		tp1.setBounds(50,50,200,200);
    	 	tp.setBounds(50,50,200,200);
-   	 	window.add(tp); 
+   	 	window.add(tp);
 	}
 	public void createPanel(){
 		panel =new JPanel();
 		panel.setLayout(null);
 		name = new JLabel("Name:");
-		name.setBounds(10, 20, 80 , 25);	
-			
+		name.setBounds(10, 20, 80 , 25);
+
 		Roll = new JLabel("Roll no.: ");
-		Roll.setBounds(10, 60, 80 , 25);			
+		Roll.setBounds(10, 60, 80 , 25);
 		Date = new JLabel("Date:");
 		Date.setBounds(250, 20, 80 , 25);
-			
+
 		D = new JLabel("12/12/12");
-		D.setBounds(300, 20, 165, 25);			
+		D.setBounds(300, 20, 165, 25);
 		N = new JLabel();
 		N.setBounds(60, 20, 165, 25);
 		R = new JLabel();
 		R.setBounds(65, 60, 165, 25);
 
-		//Setting up Buttons and their Actions		
+		//Setting up Buttons and their Actions
 		JButton Present = new JButton("Present");
 		Present.setBounds(10,100,100,25);
 		JButton Absent = new JButton("Absent");
@@ -66,11 +68,11 @@ public class GUI{
 		panel.add(name);
 		panel.add(Roll);
 		panel.add(Date);
-		
+
 		panel.add(N);
 		panel.add(R);
 		panel.add(D);
-	
+
 		panel.add(Present);
 		panel.add(Absent);
 		panel.add(Back);
@@ -78,24 +80,26 @@ public class GUI{
 		panel.add(Reset);
 		panel.add(Generate);
 		tp1.add("In Series ",panel);
-		
+
 		tp.add("Attendance",tp1);
 	}
 	public void createPanel4(){
 		panel4 =new JPanel();
 		panel4.setLayout(null);
-		JRadioButton r1=new JRadioButton("Present");    
-		JRadioButton r2=new JRadioButton("Absent");    
-		r1.setBounds(150,20,100,30);    
-		r2.setBounds(250,20,100,30); 
+		JRadioButton r1=new JRadioButton("Present");
+		JRadioButton r2=new JRadioButton("Absent");
+		r1.setBounds(150,20,100,30);
+		r2.setBounds(250,20,100,30);
+		ButtonGroup bg=new ButtonGroup();
+    bg.add(r1);bg.add(r2);
 		PArb =new JLabel("Present/Absent: ");
 		PArb.setBounds(10,20,120,25);
-		ANo = new JLabel("All Roll no. Seperated by '','' :"); 
+		ANo = new JLabel("All Roll no. Seperated by '','' :");
 		ANo.setBounds(10, 60, 220 , 25);
-		JTextArea area=new JTextArea("");  
-        	area.setBounds(225,65, 400,50); 
-        	JButton submit=new JButton("Submit");  
-    		submit.setBounds(500,125,95,30); 
+		JTextArea area=new JTextArea("");
+        	area.setBounds(225,65, 400,50);
+        	JButton submit=new JButton("Submit");
+    		submit.setBounds(500,125,95,30);
         	panel4.add(area);
         	panel4.add(submit);
 		panel4.add(PArb);
@@ -103,62 +107,78 @@ public class GUI{
 		panel4.add(r1);
 		panel4.add(r2);
 		tp1.add("By Preset/Absent",panel4);
-		
+
 	}
 	public void createPanel1(){
 		panel1 =new JPanel();
 		panel1.setLayout(null);
-		
-		SELDEP=new JLabel("Select Department Name: ");  
-		SELDEP.setBounds(10,20, 150,30);  
+
+		SELDEP=new JLabel("Select Department Name: ");
+		SELDEP.setBounds(10,20, 200,30);
 		panel1.add(SELDEP);
-		DEPNAME=new JTextField("");  
-		DEPNAME.setBounds(170,20, 250,30); 
+		DEPNAME=new JTextField("");
+		DEPNAME.setBounds(210,20, 250,30);
 		panel1.add(DEPNAME);
-		
-		SELDIV=new JLabel("Select Number of Division: ");  
-		SELDIV.setBounds(10,60, 180,30);  
+
+		SELDIV=new JLabel("Select Number of Division: ");
+		SELDIV.setBounds(10,60, 200,30);
 		panel1.add(SELDIV);
-		DEPND=new JTextField("");  
-		DEPND.setBounds(170,60, 250,30); 
+		DEPND=new JTextField("");
+		DEPND.setBounds(210,60, 250,30);
 		panel1.add(DEPND);
-		
-		SELNS=new JLabel("Select Number of Student: ");  
-		SELNS.setBounds(10,100, 180,30);  
+
+		SELNS=new JLabel("Select Number of Student: ");
+		SELNS.setBounds(10,100, 200,30);
 		panel1.add(SELNS);
-		DEPNS=new JTextField("");  
-		DEPNS.setBounds(170,100, 250,30); 
+		DEPNS=new JTextField("");
+		DEPNS.setBounds(210,100, 250,30);
 		panel1.add(DEPNS);
-		
-		JButton ADS=new JButton("Add Student");  
-		ADS.setBounds(75,140,115,30); 
+
+		JButton ADS=new JButton("Add Student");
+		ADS.setBounds(210,140,140,30);
 		panel1.add(ADS);
-		ADS.addActionListener(new ActionListener(){  
-		public void actionPerformed(ActionEvent e){  
-			JFrame frame = new JFrame("mansi");  
-			frame.setSize(400, 400);
-			
-			JPanel ADSnames=new JPanel();  
-			ADSnames.setBounds(40,80,200,200);  
-			
-			JLabel NTS=new JLabel("Name of the Student");  
-			NTS.setBounds(10, 100, 100 , 30); 
-			ADSnames.add(NTS);
-			
-			frame.add(ADSnames);	
-			frame.setVisible(true);
-			
-        }  
+		ADS.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+					createADSpanel();
+					frame.setVisible(true);
+
+        }
     }
-	);  
+	);
 		tp.add("Add Department",panel1);
 	}
-	
+	public void createADSpanel(){
+		ADSnames=new JPanel();
+		ADSnames.setLayout(null);
+		ADSnames.setBounds(10,20,200,200);
+
+		JLabel NTS=new JLabel("Name of the Student");
+		NTS.setBounds(10, 20, 150 , 30);
+		ADSnames.add(NTS);
+		EntName=new JTextField("");
+		EntName.setBounds(160,20, 200,30);
+		ADSnames.add(EntName);
+		String data[][]={ {"101","Amit"},
+                          {"102","Jai"},
+                          {"101","Sachin"}};
+    String column[]={"ID","NAME"};
+
+
+    JTable jt=new JTable(data,column);
+    jt.setBounds(10,60,350,150);
+
+		ADSnames.add(jt);
+		JButton Confirm =new JButton("Confirm");
+		Confirm.setBounds(210,250,140,30);
+		ADSnames.add(Confirm);
+
+		frame.add(ADSnames);
+	}
 	public void createPanel2(){
 		panel2 =new JPanel();
 		panel2.setLayout(null);
 		tab3 = new JLabel("Yash darling");
-		tab3.setBounds(10, 60, 80 , 25);			
+		tab3.setBounds(10, 60, 80 , 25);
 		panel2.add(tab3);
 		tp.add("View History",panel2);
 	}
@@ -166,9 +186,9 @@ public class GUI{
 		panel3 =new JPanel();
 		panel3.setLayout(null);
 		tab4 = new JLabel("Rigu ki hal");
-		tab4.setBounds(10, 60, 80 , 25);			
+		tab4.setBounds(10, 60, 80 , 25);
 		panel3.add(tab4);
 		tp.add("Help",panel3);
 	}
-	
+
 }
