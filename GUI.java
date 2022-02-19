d9b2e27bead898e0edcfef88e3f9bd606045afd5
Tejas Mayekar <tejasmayekar13@gmail.com>
2022-02-19 21:35:37 +0530

    Insert data in specific cell & Reading Data from specific cell

--- GUI.java
+++ GUI.java
@@ -9,6 +9,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import java.awt.event.*;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -24,8 +25,8 @@ public class GUI{
 	FileInputStream fis;
 	FileOutputStream fos;
 	Row row;
-	Cell cell;
-	int i=1;
+	Cell c1,wCell;
+	int i=1; 
 	public static void main(String[] args) throws EncryptedDocumentException, IOException{
 		new GUI();
 	}
@@ -56,40 +57,72 @@ public class GUI{
 		Attendace_Making_Tab=new JTabbedPane();
 		Attendace_Making_Type_Selection_tab=new JTabbedPane();
 		Attendace_Making_Type_Selection_tab.setBounds(50,50,200,200);
-   	 	Attendace_Making_Tab.setBounds(50,50,200,200);
-   	 	Main_window.add(Attendace_Making_Tab);
+		Attendace_Making_Tab.setBounds(50,50,200,200);
+		Main_window.add(Attendace_Making_Tab);
 	}
 	public void Marking_Attendance_panel(){
 		Serially_Attendace_marking =new JPanel();
 		Serially_Attendace_marking.setLayout(null);
 		name = new JLabel("Name:");
 		name.setBounds(10, 20, 80 , 25);
-
+		
 		Roll = new JLabel("Roll no.: ");
 		Roll.setBounds(10, 60, 80 , 25);
 		Date = new JLabel("Date:");
 		Date.setBounds(250, 20, 80 , 25);
-
+		
 		D = new JLabel("12/12/12");
 		D.setBounds(300, 20, 165, 25);
 		N = new JLabel();
 		N.setBounds(60, 20, 165, 25);
 		R = new JLabel();
 		R.setBounds(65, 60, 165, 25);
-
+		
 		//Setting up Buttons and their Actions
 		JButton Present = new JButton("Present");
 		Present.setBounds(10,100,100,25);
 		Present.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){	
-				N.setText("");
+				Row row=sh.getRow(i);
+				Cell c1=row.getCell(0);
+				String v =c1.toString();
+				N.setText(v);
+				try {
+					Excel_Data_Writing(1);
+				} catch (EncryptedDocumentException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				i++;
-
+				
         }
+	}
+	);
+	JButton Absent = new JButton("Absent");
+	Absent.setBounds(125,100,100,25);
+	Absent.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){	
+				row=sh.getRow(i);
+				c1=row.getCell(0);
+				String v =c1.toString();
+				N.setText(v);
+				try {
+					Excel_Data_Writing(5);
+				} catch (EncryptedDocumentException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				i++;
+				
+			}
     	}
 	  );
-		JButton Absent = new JButton("Absent");
-		Absent.setBounds(125,100,100,25);
 		JButton Back = new JButton("Back");
 		Back.setBounds(250,100,100,25);
 		JButton Next = new JButton("Next");
@@ -98,22 +131,22 @@ public class GUI{
 		Reset.setBounds(10,200,100,25);
 		Reset.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-					Reset_confirm_window_panel();
-					Reset_confirm_window.setVisible(true);
-
-        }
+				Reset_confirm_window_panel();
+				Reset_confirm_window.setVisible(true);
+				
+			}
     	}
-	  );
+		);
 		JButton Generate = new JButton("Create");
 		Generate.setBounds(365,200,100,25);
 		Serially_Attendace_marking.add(name);
 		Serially_Attendace_marking.add(Roll);
 		Serially_Attendace_marking.add(Date);
-
+		
 		Serially_Attendace_marking.add(N);
 		Serially_Attendace_marking.add(R);
 		Serially_Attendace_marking.add(D);
-
+		
 		Serially_Attendace_marking.add(Present);
 		Serially_Attendace_marking.add(Absent);
 		Serially_Attendace_marking.add(Back);
@@ -121,7 +154,7 @@ public class GUI{
 		Serially_Attendace_marking.add(Reset);
 		Serially_Attendace_marking.add(Generate);
 		Attendace_Making_Type_Selection_tab.add("In Series ",Serially_Attendace_marking);
-
+		
 		Attendace_Making_Tab.add("Marking Attendance",Attendace_Making_Type_Selection_tab);
 	}
 	public void Reset_confirm_window_panel(){
@@ -151,17 +184,17 @@ public class GUI{
 		ANo = new JLabel("All Roll no. Seperated by '','' :");
 		ANo.setBounds(10, 60, 220 , 25);
 		JTextArea area=new JTextArea("");
-        	area.setBounds(225,65, 400,50);
-        	JButton submit=new JButton("Submit");
-    		submit.setBounds(500,125,95,30);
+		area.setBounds(225,65, 400,50);
+		JButton submit=new JButton("Submit");
+		submit.setBounds(500,125,95,30);
         	Selective_Attendace_marking_panel.add(area);
         	Selective_Attendace_marking_panel.add(submit);
-		Selective_Attendace_marking_panel.add(PArb);
-		Selective_Attendace_marking_panel.add(ANo);
+			Selective_Attendace_marking_panel.add(PArb);
+			Selective_Attendace_marking_panel.add(ANo);
 		Selective_Attendace_marking_panel.add(Present_type_marking);
 		Selective_Attendace_marking_panel.add(Absent_type_marking);
 		Attendace_Making_Type_Selection_tab.add("By Preset/Absent",Selective_Attendace_marking_panel);
-
+		
 	}
 	public void Select_department_panel(){
 		panel1 =new JPanel();
@@ -173,39 +206,39 @@ public class GUI{
 		DEPNAME=new JTextField("");
 		DEPNAME.setBounds(210,20, 250,30);
 		panel1.add(DEPNAME);
-
+		
 		SELDIV=new JLabel("Select Number of Division: ");
 		SELDIV.setBounds(10,60, 200,30);
 		panel1.add(SELDIV);
 		DEPND=new JTextField("");
 		DEPND.setBounds(210,60, 250,30);
 		panel1.add(DEPND);
-
+		
 		SELNS=new JLabel("Select Number of Student: ");
 		SELNS.setBounds(10,100, 200,30);
 		panel1.add(SELNS);
 		DEPNS=new JTextField("");
 		DEPNS.setBounds(210,100, 250,30);
 		panel1.add(DEPNS);
-
+		
 		JButton ADS=new JButton("Add Student");
 		ADS.setBounds(210,140,140,30);
 		panel1.add(ADS);
 		ADS.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-					Adding_student_panel();
-					frame.setVisible(true);
-
-        }
+				Adding_student_panel();
+				frame.setVisible(true);
+				
+			}
     	}
-	  );
+		);
 		Attendace_Making_Tab.add("Add Department",panel1);
 	}
 	public void Adding_student_panel(){
 		ADSnames=new JPanel();
 		ADSnames.setLayout(null);
 		ADSnames.setBounds(10,20,200,200);
-
+		
 		JLabel NTS=new JLabel("Name of the Student");
 		NTS.setBounds(10, 20, 150 , 30);
 		ADSnames.add(NTS);
@@ -213,20 +246,20 @@ public class GUI{
 		EntName.setBounds(160,20, 200,30);
 		ADSnames.add(EntName);
 		String data[][]={ {"101","Amit"},
-                          {"102","Jai"},
-                          {"101","Sachin"}};
+		{"102","Jai"},
+		{"101","Sachin"}};
     String column[]={"ID","NAME"};
 
 
     JTable jt=new JTable(data,column);
     jt.setBounds(10,60,350,150);
-
-		ADSnames.add(jt);
-		JButton Confirm =new JButton("Confirm");
-		Confirm.setBounds(210,250,140,30);
-		ADSnames.add(Confirm);
-
-		frame.add(ADSnames);
+	
+	ADSnames.add(jt);
+	JButton Confirm =new JButton("Confirm");
+	Confirm.setBounds(210,250,140,30);
+	ADSnames.add(Confirm);
+	
+	frame.add(ADSnames);
 	}
 	public void View_Monthly_panel(){
 		panel2 =new JPanel();
@@ -236,37 +269,37 @@ public class GUI{
 		panel2.add(CHDEP);
 		String Department[]={"chaman", "bade chaman", "full chaman"};
 		cb=new JComboBox(Department);
-    cb.setBounds(180, 20,200,20);
+		cb.setBounds(180, 20,200,20);
 		panel2.add(cb);
-
+		
 		CHDIV=new JLabel("Select Division: ");
 		CHDIV.setBounds(10,60, 200,30);
 		panel2.add(CHDIV);
 		String Division[]={"chaman", "bade chaman", "full chaman"};
 		cb1= new JComboBox(Division);
-    cb1.setBounds(180, 60,200,20);
+		cb1.setBounds(180, 60,200,20);
 		panel2.add(cb1);
-
+		
 		CHNO=new JLabel("Enter Student Roll no.: ");
 		CHNO.setBounds(10,100, 200,30);
 		panel2.add(CHNO);
-
+		
 		RONO=new JTextField("");
 		RONO.setBounds(180,100, 50,30);
 		panel2.add(RONO);
-
+		
 		JButton View =new JButton("View");
 		View.setBounds(210,250,140,30);
 		View.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-					Monthly_display_box();
-					Month_wise_view_window.setVisible(true);
+				Monthly_display_box();
+				Month_wise_view_window.setVisible(true);
 
-				}
 			}
+		}
 		);
 		panel2.add(View);
-
+		
 		Attendace_Making_Tab.add("View History",panel2);
 	}
 	public void Monthly_display_box(){
@@ -279,55 +312,56 @@ public class GUI{
 		month2.setBounds(10,100,200,25);
 		HIS.add(month1);
 		HIS.add(month2);
-
-		Jan = new JLabel("0");
+		
+		
+		Jan = new JLabel("");
 		Jan.setBounds(10,60,20,25);
 		HIS.add(Jan);
-
+		
 		Feb = new JLabel("0");
 		HIS.add(Feb);
 		Feb.setBounds(40,60,20,25);
-
+		
 		Mar = new JLabel("0");
 		Mar.setBounds(80,60,20,25);
 		HIS.add(Mar);
-
+		
 		Apr = new JLabel("0");
 		Apr.setBounds(110,60,20,25);
 		HIS.add(Apr);
-
+		
 		May = new JLabel("0");
 		May.setBounds(150,60,20,25);
 		HIS.add(May);
-
+		
 		Jun = new JLabel("0");
 		Jun.setBounds(180,60,20,25);
 		HIS.add(Jun);
-
+		
 		Jul = new JLabel("0");
 		Jul.setBounds(10,140,20,25);
 		HIS.add(Jul);
-
+		
 		Aug = new JLabel("0");
 		HIS.add(Aug);
 		Aug.setBounds(40,140,20,25);
-
+		
 		Sep = new JLabel("0");
 		Sep.setBounds(80,140,20,25);
 		HIS.add(Sep);
-
+		
 		Oct = new JLabel("0");
 		Oct.setBounds(110,140,20,25);
 		HIS.add(Oct);
-
+		
 		Nov = new JLabel("0");
 		Nov.setBounds(150,140,20,25);
 		HIS.add(Nov);
-
+		
 		Dec = new JLabel("0");
 		Dec.setBounds(180,140,20,25);
 		HIS.add(Dec);
-
+		
 		Month_wise_view_window.add(HIS);
 	}
 	public void Help_Panel(){
@@ -342,16 +376,27 @@ public class GUI{
 		fis = new FileInputStream("./Test.xlsx");
 		wb = WorkbookFactory.create(fis);
 		sh = wb.getSheet("sheet1");
-		int no_of_row = sh.getLastRowNum();
+		int no_of_row =sh.getLastRowNum();
 		System.out.println(no_of_row);
 		
-		Row row=sh.getRow(1);
-		Cell c1=row.getCell(0);
-		String v =c1.toString();
-
-		System.out.println(v);
-		
-		System.out.println("done");
 	}
-
+	public void Excel_Data_Writing(int Input) throws EncryptedDocumentException, IOException{
+		fis = new FileInputStream("./Test.xlsx");
+		wb =WorkbookFactory.create(fis);
+		sh =wb.getSheet("sheet1");
+		row =sh.getRow(i);
+		wCell=row.createCell(1);
+		wCell.setCellValue(Input);
+		fos = new FileOutputStream("./Test.xlsx");
+		wb.write(fos);
+		fos.flush();
+		fos.close();
+		int no_of_row = sh.getLastRowNum();
+		if(i==no_of_row){
+			N.setText("Completed");
+		}
+		System.out.println("Done");
+		
+	}
+	
 }
